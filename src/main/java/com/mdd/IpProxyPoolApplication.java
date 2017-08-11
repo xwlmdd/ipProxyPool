09646699a08572db520aa1cb9eef0dbde0bf4dbb
wanglu_xu <wanglu_xu@suishouji.com>
2017-08-11 13:04:07 +0800

    Initial commit

---
+++ src/main/java/com/mdd/IpProxyPoolApplication.java
@@ -0,0 +1,12 @@
+package com.mdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpProxyPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpProxyPoolApplication.class, args);
+	}
+}
