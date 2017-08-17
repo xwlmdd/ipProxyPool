25e479be84577265a31cb1fd5e4a1d8911f55669
xwlmdd <1072552712@qq.com>
2017-08-18 00:12:11 +0800

    米扑代理模拟登入

--- src/main/java/com/mdd/proxyip/login/MiPuCrawlerLogin.java
+++ src/main/java/com/mdd/proxyip/login/MiPuCrawlerLogin.java
@@ -1,14 +1,49 @@
 package com.mdd.proxyip.login;
 
+import com.mdd.proxyip.utils.HttpClientUtils;
+import com.mdd.proxyip.utils.HttpRequestData;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
+
+import java.util.HashMap;
 
 /**
  * Created by xwl on 2017/8/16.
  */
 public class MiPuCrawlerLogin {
 
+    private static final String LOGIN_URL = "http://proxy.mimvp.com/lib/user_login_check.php";
 
+    public String login() {
+        HttpRequestData httpRequestData = new HttpRequestData();
+        httpRequestData.setRequestMethod("POST");
+        //请求头
+        HashMap<String, String> headerMap = new HashMap<>();
+        headerMap.put("Host","proxy.mimvp.com");
+        headerMap.put("Connection","keep-alive");
+        headerMap.put("Content-Length","58");
+        headerMap.put("Accept","application/json, text/javascript, */*; q=0.01");
+        headerMap.put("Host","http://proxy.mimvp.com");
+        headerMap.put("Origin","proxy.mimvp.com");
+        headerMap.put("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36");
+        headerMap.put("X-Requested-With","XMLHttpRequest");
+        headerMap.put("Referer","http://proxy.mimvp.com/usercenter/login.php");
+        headerMap.put("Accept-Encoding","gzip, deflate");
+        headerMap.put("Accept-Language","zh-CN,zh;q=0.8");
+        //请求参数
+        HashMap<String, String> paramsMap = new HashMap<>();
+        paramsMap.put("user_email","1072552712@qq.com");
+        paramsMap.put("user_pwd","yzq0914");
+        paramsMap.put("remember","1");
+        httpRequestData.setHeaders(headerMap);
+        httpRequestData.setParams(paramsMap);
+        httpRequestData.setRequestUrl(LOGIN_URL);
+        HttpResponse httpResponse = HttpClientUtils.execute(httpRequestData);
+        System.out.println(httpResponse.getStatusLine().getStatusCode());
+        return "";
+    }
+
+    public static void main(String[] args) {
+        new MiPuCrawlerLogin().login();
+    }
 
 }
--- src/main/java/com/mdd/proxyip/utils/HttpClientUtils.java
+++ src/main/java/com/mdd/proxyip/utils/HttpClientUtils.java
@@ -1,14 +1,15 @@
 package com.mdd.proxyip.utils;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -26,29 +27,65 @@ public class HttpClientUtils {
 
     public static HttpClient httpClient = HttpClients.createDefault();
 
-    public static HttpResponse postRequest(HttpRequestData httpRequestData) {
-        if (httpRequestData != null && "post".equalsIgnoreCase(httpRequestData.getRequestMethod())) {
+    public static HttpResponse execute(HttpRequestData httpRequestData) {
+        HttpResponse httpResponse = null;
+        //请求方式
+        String requestMethod = httpRequestData.getRequestMethod();
+        //请求头
+        Header[] headers = mapConvertHeader(httpRequestData.getHeaders());
+        if (httpRequestData != null && "post".equalsIgnoreCase(requestMethod)) {
             HttpPost httpPost = new HttpPost();
-            httpPost.setHeaders(mapConvertHeader(httpRequestData.getHeaders()));
+            httpPost.setHeaders(headers);
             List<BasicNameValuePair> basicNameValuePairs = mapCovertBasicNameValuePair(httpRequestData.getParams());
             HttpEntity entity = null;
             try {
                 entity = new UrlEncodedFormEntity(basicNameValuePairs, "utf-8");
                 httpPost.setEntity(entity);
-                HttpResponse httpResponse = httpClient.execute(httpPost);
+                httpResponse = httpClient.execute(httpPost);
             } catch (UnsupportedEncodingException e) {
                 logger.error("构造参数失败！", e);
             } catch (IOException e) {
                 logger.error("请求失败！", e);
             }
 
+        }else if ("get".equalsIgnoreCase(requestMethod)){
+            String url = httpRequestData.getRequestUrl() + "?" + covertParams(httpRequestData.getParams());
+            logger.info("get请求地址："+url);
+            HttpGet httpGet = new HttpGet(url);
+            httpGet.setHeaders(headers);
+            try {
+                httpResponse = httpClient.execute(httpGet);
+            } catch (IOException e) {
+                logger.error("请求失败！", e);
+            }
+
         }
-        return null;
+        return httpResponse;
+    }
+
+    /**
+     * get请求参数转换
+     * @param paramMap
+     * @return
+     */
+    private static String covertParams(Map<String, String> paramMap){
+        if (paramMap == null) {
+            logger.info("请求参数为空！");
+        }
+        StringBuffer paramString = new StringBuffer();
+        for (Map.Entry<String, String> entry : paramMap.entrySet()){
+            paramString = paramString.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+        }
+        String params = paramString.toString();
+        if (StringUtils.isNotBlank(params)&&params.equals("&")){
+            params = params.substring(0,params.lastIndexOf("&"));
+        }
+        return params;
     }
 
     /**
      * post请求参数转换
-     *
+     * @param paramMap
      * @return
      */
     private static List<BasicNameValuePair> mapCovertBasicNameValuePair(Map<String, String> paramMap) {
@@ -65,7 +102,7 @@ public class HttpClientUtils {
 
     /**
      * map转Header数组
-     *
+     * @param headerMap
      * @return
      */
     private static Header[] mapConvertHeader(Map<String, String> headerMap) {
